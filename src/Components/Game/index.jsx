075af13e2bd22ff4af99656fb84f1fe14944fe0b
stitaks <sarka.stitak@gmail.com>
2023-12-02 12:54:09 +0100

    uprava game

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,10 +1,12 @@
-
+import './style.css'
 import { Card } from "../Card"
 
 export const Game = (props) => {
     return (
-        props.game.images.map((image) => (
-            <Card key={props.name} name={image}/>
-        ))
+        <div className="game">
+            {props.game.images.map((image) => (
+                <Card key={image} name={image}/>
+            ))}
+        </div>
     )
 }
