84c004996750ba2501c7605ce800a21cf94bd087
stitaks <sarka.stitak@gmail.com>
2023-12-02 08:06:44 +0100

    zalozeni projektu

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <h1>Pohádkové pexeso</h1>
+      <p></p>
+      <button >Tři prasátka</button>
+    </div>
+  );
+};
