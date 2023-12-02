7f405ff1abf1801256457b6bd4c02d09d2ae1428
Kateryna Medvid <cruassan777@gmail.com>
2023-12-02 11:42:45 +0100

    wraper

--- src/Components/Welcome/index.jsx
+++ src/Components/Welcome/index.jsx
@@ -1,16 +1,18 @@
-import { fairytails } from '../../Constants'
-import { Button } from '../Button'
-import './style.css'
-
+import { fairytails } from '../../Constants';
+import { Button } from '../Button';
+import './style.css';
 
 export const Welcome = () => {
-    return (
-        <div className="welcomeWindows">
-            <h1>Pohádkové pexeso</h1>
-            <p></p>
-            
-            { Object.keys(fairytails).map((nameTails => <Button key={nameTails} name={fairytails[nameTails].name}/>))}
-            
-        </div>
-    )
-}
+  return (
+    <div className="wraperWelcomeWindows">
+      <div className="WelcomeWindows">
+        <h1>Pohádkové pexeso</h1>
+        <p></p>
+
+        {Object.keys(fairytails).map((nameTails) => (
+          <Button key={nameTails} name={fairytails[nameTails].name} />
+        ))}
+      </div>
+    </div>
+  );
+};
