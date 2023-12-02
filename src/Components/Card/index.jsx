d16fa4b8d075f9c48ec34e56290dac2b6976e7fd
Kateryna Medvid <cruassan777@gmail.com>
2023-12-02 16:47:42 +0100

    animace cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,28 +1,29 @@
 import { useEffect, useState } from 'react';
 import './style.css';
 
-export const Card = ({card}) => {
-    const [isReverse, setIsReverse] = useState(card.reverse)
-    const [showAnimation, setShowAnimation] = useState(false);
+export const Card = ({ card }) => {
+  const [isReverse, setIsReverse] = useState(card.reverse);
+  const [showAnimation, setShowAnimation] = useState(false);
 
-    useEffect(() => {
-        card.reverse = true;
-    }, [isReverse])
+  useEffect(() => {
+    card.reverse = true;
+  }, [isReverse]);
 
   const handleClick = () => {
-    setShowAnimation(true)
+    setShowAnimation(true);
     setTimeout(() => {
-        setIsReverse(true);
-        setTimeout(()=>{
-            setShowAnimation(false)
-        }, 500)
-    }, 500)
-    
+      setIsReverse(true);
+      setTimeout(() => {
+        setShowAnimation(false);
+      }, 500);
+    }, 500);
   };
 
   return (
     <img
-    className={`cardImg${showAnimation ? ' animate' : ''}`}
+      className={`cardImg${showAnimation ? ' animate' : ''}${
+        isReverse ? ' back' : ' front'
+      }`}
       onClick={handleClick}
       src={isReverse ? `../../img/${card.image}` : '../../img/zadnistrana.png'}
       alt=""
