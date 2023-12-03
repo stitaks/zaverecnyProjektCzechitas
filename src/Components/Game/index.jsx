9446faf9320f33632c0e9f6564dd257d39b49a28
Kateryna Medvid <cruassan777@gmail.com>
2023-12-03 13:23:37 +0100

    pridane tlacitko

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,87 +1,91 @@
-import './style.css'
-import { Card } from "../Card"
-import { fairytails } from "../../Constants/index"
-import { useEffect, useRef, useState } from 'react'
+import './style.css';
+import { Card } from '../Card';
+import { fairytails } from '../../Constants/index';
+import { useEffect, useRef, useState } from 'react';
+import { Button } from '../Button';
 
 export const Game = (props) => {
-    const [cards, setCards] = useState(props.game.cards.map((card) => {
+  const [cards, setCards] = useState(
+    props.game.cards
+      .map((card) => {
         return {
-            ...card,
-            order: Math.random() * 100,
-        }
-    }).sort((a,b) => a.order - b.order));
-    const [openCards, setOpenCards] = useState([]);
+          ...card,
+          order: Math.random() * 100,
+        };
+      })
+      .sort((a, b) => a.order - b.order),
+  );
+  const [openCards, setOpenCards] = useState([]);
 
-    const isActiveRef = useRef(true)
+  const isActiveRef = useRef(true);
 
-    useEffect(() => {
-        if (openCards.length === 2) {
-            isActiveRef.current = false;
+  useEffect(() => {
+    if (openCards.length === 2) {
+      isActiveRef.current = false;
 
-            if (openCards[0].image !== openCards[1].image) {
-                setTimeout(() => {
-                    setCards((previousCard) => {
-                        return previousCard.map((card) => {
-                            if (card.id === openCards[0].id) {
-                                return {
-                                    ...card,
-                                    reverse: false,
-                                };
-                            }
-                            if (card.id === openCards[1].id) {
-                                return {
-                                    ...card,
-                                    reverse: false,
-                                };
-                            }
+      if (openCards[0].image !== openCards[1].image) {
+        setTimeout(() => {
+          setCards((previousCard) => {
+            return previousCard.map((card) => {
+              if (card.id === openCards[0].id) {
+                return {
+                  ...card,
+                  reverse: false,
+                };
+              }
+              if (card.id === openCards[1].id) {
+                return {
+                  ...card,
+                  reverse: false,
+                };
+              }
 
-                            return card;
-                        })
-                    })
-                    isActiveRef.current = true;
-                }, 2000)
-            }
-            else {
-                isActiveRef.current = true;
-            }
-            setOpenCards([]);
-        }
-    }, [openCards])
+              return card;
+            });
+          });
+          isActiveRef.current = true;
+        }, 2000);
+      } else {
+        isActiveRef.current = true;
+      }
+      setOpenCards([]);
+    }
+  }, [openCards]);
 
+  return (
+    <>
+      <Button nameTails="" name="Zpět" />
+      <div className="game">
+        {cards.map((card, index) => (
+          <Card
+            key={`${card.id}_${card.reverse}`}
+            card={card}
+            onClick={() => {
+              if (!isActiveRef.current) {
+                return;
+              }
+              const newCards = cards.map((newCard) => {
+                if (newCard.id === card.id) {
+                  return {
+                    ...newCard,
+                    reverse: !newCard.reverse ? true : true,
+                  };
+                }
+                return newCard;
+              });
 
-    return (
-        <>
-        <div className='back'>Back</div>
-        <div className="game">
-            {cards.map((card, index) => (
-                <Card
-                    key={`${card.id}_${card.reverse}`}
-                    card={card}
-                    onClick={() => {
-                        if(!isActiveRef.current){
-                            return;
-                        }
-                        const newCards = cards.map((newCard) => {
-                            if (newCard.id === card.id) {
-                                return {
-                                    ...newCard,
-                                    reverse: !newCard.reverse ? true : true
-                                };
-                            }
-                            return newCard;
-                        });
+              console.log(newCards);
 
-                        console.log(newCards)
+              if (!card.reverse) {
+                setOpenCards([...openCards, card]);
+              }
 
-                        if (!card.reverse) {
-                            setOpenCards([...openCards, card])
-                        }
-
-                        setCards(newCards)
-                    }}
-                    setOpenCards={setOpenCards} />
-            ))}
-        </div>
-        </>
-    )
-}
+              setCards(newCards);
+            }}
+            setOpenCards={setOpenCards}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
