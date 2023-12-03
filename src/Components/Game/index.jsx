04639c4be2e9be2fe5fc12d48d4439507f824280
stitaks <sarka.stitak@gmail.com>
2023-12-03 13:56:01 +0100

    pridani ohnostroje

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,18 +1,30 @@
 import './style.css'
 import { Card } from "../Card"
-import { fairytails } from "../../Constants/index"
 import { useEffect, useRef, useState } from 'react'
+import { Fireworks } from '@fireworks-js/react'
+
+
+const randomizeCards = (cards) => {
+    return (
+        cards.map((card) => {
+            return {
+                ...card,
+                order: Math.random() * 100,
+            }
+        }).sort((a, b) => a.order - b.order));
+}
 
 export const Game = (props) => {
-    const [cards, setCards] = useState(props.game.cards.map((card) => {
-        return {
-            ...card,
-            order: Math.random() * 100,
-        }
-    }).sort((a,b) => a.order - b.order));
+    const [cards, setCards] = useState(randomizeCards(props.game.cards));
     const [openCards, setOpenCards] = useState([]);
+    const [isFinished, setIsFinished] = useState(true)
 
     const isActiveRef = useRef(true)
+
+    useEffect(() => {
+        setOpenCards([])
+        setCards(randomizeCards(props.game.cards))
+    }, [props.game])
 
     useEffect(() => {
         if (openCards.length === 2) {
@@ -43,45 +55,68 @@ export const Game = (props) => {
             }
             else {
                 isActiveRef.current = true;
+                if (cards.every((card) => card.reverse)) {
+                    setIsFinished(true)
+                }
             }
             setOpenCards([]);
         }
     }, [openCards])
 
+    const ref = useRef(null)
+
+    const toggle = () => {
+        if (!ref.current) return
+        if (ref.current.isRunning) {
+            ref.current.stop()
+        } else {
+            ref.current.start()
+        }
+    }
+
 
     return (
         <>
-        <div className='back'>Back</div>
-        <div className="game">
-            {cards.map((card, index) => (
-                <Card
-                    key={`${card.id}_${card.reverse}`}
-                    card={card}
-                    onClick={() => {
-                        if(!isActiveRef.current){
-                            return;
-                        }
-                        const newCards = cards.map((newCard) => {
-                            if (newCard.id === card.id) {
-                                return {
-                                    ...newCard,
-                                    reverse: !newCard.reverse ? true : true
-                                };
+            <div className='back'>Back</div>
+            <div className="game">
+                {!isFinished ? (cards.map((card, index) => (
+                    <Card
+                        key={`${card.id}_${card.reverse}`}
+                        card={card}
+                        onClick={() => {
+                            if (!isActiveRef.current) {
+                                return;
                             }
-                            return newCard;
-                        });
+                            const newCards = cards.map((newCard) => {
+                                if (newCard.id === card.id) {
+                                    return {
+                                        ...newCard,
+                                        reverse: !newCard.reverse ? true : true
+                                    };
+                                }
+                                return newCard;
+                            });
 
-                        console.log(newCards)
+                            if (!card.reverse) {
+                                setOpenCards([...openCards, card])
+                            }
 
-                        if (!card.reverse) {
-                            setOpenCards([...openCards, card])
-                        }
-
-                        setCards(newCards)
+                            setCards(newCards)
+                        }}
+                        setOpenCards={setOpenCards} />
+                ))) : (<Fireworks
+                    ref={ref}
+                    options={{ opacity: 0.5 }}
+                    style={{
+                      top: '100px',
+                      left: 0,
+                      width: '100%',
+                      height: '100%',
+                      position: 'fixed',
                     }}
-                    setOpenCards={setOpenCards} />
-            ))}
-        </div>
+                  />
+            )}
+            </div>
         </>
     )
 }
